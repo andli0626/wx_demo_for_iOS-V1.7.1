796db2d48a8ccd543bca6f97486d1cca9ca17a3b
李林 <ww1095@163.com>
2016-06-17 10:19:37 +0800

    init 无错版本

---
+++ wx_demo_for_iOS V1.7.1/ViewController/SendMsgToWeChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendMsgToWeChatViewController.h
+//  ApiClient
+//
+//  Created by Tencent on 12-2-27.
+//  Copyright (c) 2012年 Tencent. All rights reserved.
+//  首页
+
+#import <UIKit/UIKit.h>
+
+@interface SendMsgToWeChatViewController : UIViewController
+
+@end
